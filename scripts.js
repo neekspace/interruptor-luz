765c8ecda3cb7d67ae69ee07c0d2792fab9402f3
Nicole Soares <nicsangernic@gmail.com>
2024-12-30 18:28:38 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const background = document.querySelector("body"),
+    button = document.querySelector("div.switch");
+
+button.addEventListener("click", function () {
+  if (background.className === "") {
+    background.classList.add("night");
+  } else {
+    background.classList.remove("night");
+  }
+});
